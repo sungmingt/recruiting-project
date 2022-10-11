ae6188e265b0699abf600c310aef94e42b8a1b21
sungmingt <qotjdals8313@naver.com>
2022-10-11 21:35:50 +0900

    FEAT : apply Swagger API Docs

---
+++ src/main/java/project/recruiting/config/SwaggerConfig.java
@@ -0,0 +1,33 @@
+package project.recruiting.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import springfox.documentation.builders.ApiInfoBuilder;
+import springfox.documentation.builders.PathSelectors;
+import springfox.documentation.builders.RequestHandlerSelectors;
+import springfox.documentation.service.ApiInfo;
+import springfox.documentation.spi.DocumentationType;
+import springfox.documentation.spring.web.plugins.Docket;
+
+@Configuration
+public class SwaggerConfig {
+
+    @Bean
+    public Docket api() {
+        return new Docket(DocumentationType.OAS_30)
+                .useDefaultResponseMessages(false)
+                .select()
+                .apis(RequestHandlerSelectors.basePackage("project.recruiting"))
+                .paths(PathSelectors.any())
+                .build()
+                .apiInfo(apiInfo());
+    }
+
+    private ApiInfo apiInfo() {
+        return new ApiInfoBuilder()
+                .title("기업 채용 웹 서비스 API (Recruiting Service)")
+                .description("API Docs for Recruiting Service")
+                .version("1.0")
+                .build();
+    }
+}
--- src/main/java/project/recruiting/web/recruit/controller/RecruitController.java
+++ src/main/java/project/recruiting/web/recruit/controller/RecruitController.java
@@ -1,5 +1,6 @@
 package project.recruiting.web.recruit.controller;
 
+import io.swagger.v3.oas.annotations.Operation;
 import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.*;
 import project.recruiting.domain.recruit.service.RecruitService;
@@ -12,33 +13,25 @@ import java.util.List;
 
 @RestController
 @RequiredArgsConstructor
+@RequestMapping("/recruit")
 public class RecruitController {
 
     private final RecruitService recruitService;
 
-    /**
-     * 채용공고 등록
-     * @require 회사_id, position, reward, content, tool
-     */
+    @Operation(summary = "채용공고 등록")
     @PostMapping
     public RegisterResponse register(@RequestBody RegisterRequest request) {
         return recruitService.register(request);
     }
 
-    /**
-     * 채용공고 수정
-     * @require position, reward, content, tool
-     */
+    @Operation(summary = "채용공고 수정")
     @PostMapping("/{recruitId}")
     public UpdateResponse update(@PathVariable long recruitId,
                                  @RequestBody UpdateRequest request) {
         return recruitService.update(recruitId, request);
     }
 
-    /**
-     * 채용공고 삭제
-     * @require recruit_id
-     */
+    @Operation(summary = "채용공고 삭제")
     @DeleteMapping("/{recruitId}")
     public String delete(@PathVariable long recruitId) {
 
@@ -46,43 +39,25 @@ public class RecruitController {
         return "채용공고가 삭제되었습니다.";
     }
 
-    /**
-     * 채용공고 목록
-     * @return recruit_id, company_name, country, city, position, reward, tool
-     */
+    @Operation(summary = "채용공고 목록")
     @GetMapping
-    public MultiResponse getList() {
-
-        List<ListResponse> response = recruitService.getList();
-        return new MultiResponse(response);
+    public List<ListResponse> getList() {
+        return recruitService.getList();  //todo : 리스트 dto로 한번 더 감싸야 할지
     }
 
-    /**
-     * 채용공고 검색
-     * @return recruit_id, company_name, country, city, position, reward, tool
-     */
-    @GetMapping
-    public ListResponse search(@RequestParam String content) {  //todo : 검색 조건 추가
-
-        recruitService.search(content);
-//        return new MultiResponse(listResponse);
-        return new ListResponse();
+    @Operation(summary = "채용공고 검색")
+    @GetMapping("/search")
+    public List<ListResponse> search(@RequestParam String content) {  //todo : 검색 조건 추가
+        return recruitService.search(content);
     }
 
-
-    /**
-     * 채용공고 상세 페이지
-     * @return recruit_id, company_name, country, city, position, reward, tool, content, 해당 회사가 올린 다른 채용공고 (id List)
-     */
+    @Operation(summary = "채용공고 상세 페이지")
     @GetMapping("/{recruitId}")
     public SingleResponse getSingle(@PathVariable long recruitId) {
         return recruitService.getSingle(recruitId);
     }
 
-    /**
-     * 채용공고 지원
-     * @require recruit_id, user_id
-     */
+    @Operation(summary = "채용공고 지원")
     @PostMapping("/apply")
     public String apply(@RequestBody ApplyRequest applyDto) {
 
