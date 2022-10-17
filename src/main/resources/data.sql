e835077d6d0b44ec983b19cc5915afa943cf3128
sungmingt <qotjdals8313@naver.com>
2022-10-17 23:11:42 +0900

    FEAT : add data.sql

---
+++ src/main/resources/data.sql
@@ -0,0 +1,6 @@
+INSERT INTO COMPANY VALUES (1, 'Seoul', 'South Korea', 'Naver');
+INSERT INTO COMPANY VALUES (2, 'San Francisco', 'America', 'Google');
+INSERT INTO COMPANY VALUES (3, 'Seoul', 'South Korea', 'Kakao');
+INSERT INTO COMPANY VALUES (4, 'New York', 'America', 'BOA');
+
+INSERT INTO MEMBER VALUES (1, '김코딩', null);
